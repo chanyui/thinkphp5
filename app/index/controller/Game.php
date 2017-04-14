15ca603e0ca193e92d0d7b21a46b46fbcb44aa01
ycheng <718653142@qq.com>
2017-04-14 17:30:30 +0800

    add Game.php

--- app/index/controller/Game.php
+++ app/index/controller/Game.php
@@ -14,7 +14,28 @@ class Game extends Controller
 
     public function index()
     {
-        return $this->fetch();
+        if (request()->isPost()) {
+            dump(session('numArr'));
+            $data = input('post.');
+            dump($data);
+        } else {
+            return $this->fetch();
+        }
     }
+
+    public function getNum()
+    {
+        $tmp = array();
+        while (count($tmp) < 4) {
+            $tmp[] = mt_rand(0, 9);
+            $tmp = array_unique($tmp);
+        }
+        $randNum = $tmp;
+        session('numArr',$randNum);
+        $this->redirect('game/index');
+    }
+
+
+
 
 }
