e7234c4be0554265137bd207593b427d141c60c2
ycheng <718653142@qq.com>
2017-07-03 16:13:08 +0800

    aliyun-oss sdk

---
+++ vendor/aliyun-oss/samples/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class Config
+ *
+ * 执行Sample示例所需要的配置，用户在这里配置好Endpoint，AccessId， AccessKey和Sample示例操作的
+ * bucket后，便可以直接运行RunAll.php, 运行所有的samples
+ */
+final class Config
+{
+    const OSS_ACCESS_ID = ''; //0AJhW6zCwabpC2PG
+    const OSS_ACCESS_KEY = ''; //piUuMw0oK37nIiXB6jJOE3AhJNZUoz
+    const OSS_ENDPOINT = ''; //http://oss-cn-hangzhou.aliyuncs.com
+    const OSS_TEST_BUCKET = '';
+}
