2a8b1de47f87af76eac389314223f34d1bb75950
yc <718653142@qq.com>
2019-09-11 11:35:36 +0800

    增加公共方法

--- app/common.php
+++ app/common.php
@@ -1201,6 +1201,78 @@ if (!function_exists('_getMysqlBatchUpdateStatement')) {
     }
 }
 
+/**
+ * 批量更新函数
+ * @param $data array 待更新的数据，二维数组格式
+ * @param array $params array 值相同的条件，键值对应的一维数组
+ * @param string $field string 值不同的条件，默认为id
+ * @return bool|string
+ */
+if (function_exists('batchUpdate')) {
+    function batchUpdate($data, $field, $params = [])
+    {
+        if (!is_array($data) || !$field || !is_array($params)) {
+            return false;
+        }
+
+        $updates = parseUpdate($data, $field);
+        $where = parseParams($params);
+
+        // 获取所有键名为$field列的值，值两边加上单引号，保存在$fields数组中
+        // array_column()函数需要PHP5.5.0+，如果小于这个版本，可以自己实现，
+        // 参考地址：http://php.net/manual/zh/function.array-column.php#118831
+        $fields = array_column($data, $field);
+        $fields = implode(',', array_map(function ($value) {
+            return "'" . $value . "'";
+        }, $fields));
+
+        $sql = sprintf("UPDATE `%s` SET %s WHERE `%s` IN (%s) %s", 'post', $updates, $field, $fields, $where);
+
+        return $sql;
+    }
+}
+
+/**
+ * 将二维数组转换成CASE WHEN THEN的批量更新条件
+ * @param $data array 二维数组
+ * @param $field string 列名
+ * @return string sql语句
+ */
+if (function_exists('parseUpdate')) {
+    function parseUpdate($data, $field)
+    {
+        $sql = '';
+        $keys = array_keys(current($data));
+        foreach ($keys as $column) {
+            $sql .= sprintf("`%s` = CASE `%s` \n", $column, $field);
+            foreach ($data as $line) {
+                $sql .= sprintf("WHEN '%s' THEN '%s' \n", $line[$field], $line[$column]);
+            }
+            $sql .= "END,";
+        }
+
+        return rtrim($sql, ',');
+    }
+}
+
+/**
+ * 解析where条件
+ * @param $params
+ * @return array|string
+ */
+if (function_exists('parseParams')) {
+    function parseParams($params)
+    {
+        $where = [];
+        foreach ($params as $key => $value) {
+            $where[] = sprintf("`%s` = '%s'", $key, $value);
+        }
+
+        return $where ? ' AND ' . implode(' AND ', $where) : '';
+    }
+}
+
+
 // +----------------------------------------------------------------------
 // | Author: Lance Li
 // +----------------------------------------------------------------------
@@ -1755,5 +1827,25 @@ if (!function_exists('listSortBy')) {
             return $resultSet;
         }
         return [];
+    }
+}
+
+/**
+ * 转化树结构为列表结构
+ */
+if (!function_exists('changeTreeToListByPid')) {
+    function changeTreeToListByPid($tree, $field)
+    {
+        if (empty($tree)) {
+            return [];
+        }
+        $list = [];
+        foreach ($tree as $key => $value) {
+            $childList = changeTreeToListByPid($value[$field], $field);
+            unset($value[$field]);
+            $list[] = $value;
+            $list = array_merge($list, $childList);
+        }
+        return $list;
     }
 }
